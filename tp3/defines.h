25ea64c5e91fe20524143e0861f0eba53f439be8
msramalho <msramalho>
2017-10-23 10:11:35 +0100

    defines.h for reuse across sender and receiver

---
+++ tp3/defines.h
@@ -0,0 +1,15 @@
+
+#define FALSE 0
+#define TRUE 1
+//llopen defines
+#define FLAG 0x7E
+#define FLAG_R1 0x7D
+#define FLAG_R2 0x5E
+#define FLAG_R3 0x5D
+#define A 0x03
+#define C_SET 0x03
+#define C_UA 0x07
+#define C_S0 0x00
+#define C_S1 0x40
+#define RR_0 0x05 //0b00000101
+#define RR_1 0x85 //0b10000101
