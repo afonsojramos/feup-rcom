eff347076028e2a0017cca0c27574db5d8e79a9a
msramalho <up201403027@fe.up.pt>
2017-10-26 11:22:19 +0100

    applet interface

--- tp3/applet.c
+++ tp3/applet.c
@@ -7,6 +7,9 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <signal.h>
+#include <strings.h>
+
+#include "defines.h"
 
 typedef struct{
 	int fd_port;
@@ -15,34 +18,75 @@ typedef struct{
 	int fd_file_create;
 } config;
 
-int main(int argc, char **argv){
+char sendFile(int fd, char * filename);
 
+int main(int argc, char **argv){
+	struct termios oldtio,newtio;
   config cfg = { .fd_port = 0, .fd_file_send = 0, .fd_file_create = 0};
 
 	printf("Sender(1) or Receiver(0)?\n");
+	int stat;
 	scanf("%d", &stat);
 	printf("%d\n", stat);
 
-	if(stat == 0 || stat == 1){
+	if(stat != 0 && stat != 1){
+		printf("Invalid instruction (0 for sender and 1 for receiver)\n");
+		exit(1);
+	}
 
+	if(stat == 1 && argc != 2){
+		printf("No file specified in the command arguments\nUsage: ./applet <fileToSend>\n");
+		exit(2);
 	}
 
 	//Choose port
 	printf("\n Which PORT do you want to use? \n(0)--(/dev/ttyS0)  (1)--(/dev/ttyS1).\n");
+	char option;
 	scanf(" %c", &option);
 	switch(option){
 	  case '0': strcpy(cfg.port,"/dev/ttyS0"); break;
 	  case '1': strcpy(cfg.port,"/dev/ttyS1"); break;
+		default: printf("Invalid port chosen\n"); exit(3);
 	}
 
 	//Open port
 	cfg.fd_port = open(cfg.port, O_RDWR | O_NOCTTY );
 	if (cfg.fd_port <0){
-	    return ERROR;
+			perror(cfg.port);
+	    return -1;
 	}
 
-	if ( tcgetattr(cfg.fd_port,&oldtio) == -1){
+	if (tcgetattr(cfg.fd_port,&oldtio) == -1){
 	    printf("\nError reading port settings.\n");
 	    return -1;
 	}
+
+
+    bzero(&newtio, sizeof(newtio));
+    newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
+    newtio.c_iflag = IGNPAR;
+    newtio.c_oflag = 0;
+
+    /* set input mode (non-canonical, no echo,...) */
+    newtio.c_lflag = 0;
+
+    newtio.c_cc[VTIME] = 0;   /* inter-character timer unused */
+    newtio.c_cc[VMIN] = 1;   /* blocking read until 1 char received */
+
+    tcflush(cfg.fd_port, TCIOFLUSH);
+
+    if (tcsetattr(cfg.fd_port, TCSANOW, &newtio) == -1) {
+      perror("tcsetattr failed");
+	    return -1;
+    }
+
+    printf("Ready to send\n");
+
+		char result = sendFile(cfg.fd_port, argv[1]);
+
+}
+
+char sendFile(int fd, char * filename){
+	printf("sending file %s.\n", filename);
+	return TRUE;
 }
