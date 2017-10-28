5b29d6c8432135a7200ed2c5d10fac11d24dc696
Daniel Silva <danny@dannyps.net>
2017-10-28 18:37:50 +0100

    We should be nearing the 'fim de diarreia gittiana'.

--- tp3/app_s_utils.c
+++ tp3/app_s_utils.c
@@ -2,6 +2,7 @@
 #include "defines.h"
 #include "sender_utils.c"
 
+int sendData(int fd, char* bytes, unsigned int n);
 
 int sendControl(int fd, unsigned char start, char *fileName, unsigned int fileSize)
 {
@@ -27,11 +28,12 @@ int sendControl(int fd, unsigned char start, char *fileName, unsigned int fileSi
 
     bytes[currPos]=1; // here we signal TYPE = 1 (file name)
     len=strlen(fileName);
+	DEBUG_PRINT("file name len=%d\n", len);
     bytes[++currPos]=len; // pass file name string length here
-    memcpy(bytes+currPos, fileName, len); // copy file name string to bytes to send.
+    memcpy(bytes+currPos+1, fileName, len); // copy file name string to bytes to send.
 
-    currPos+=len; // update currPos
-
+    currPos+=len+1; // update currPos
+	printB(bytes, currPos);
     // HOORAY -- our packet is complete! Sending...
 
     return llwriteS(fd, bytes, currPos);
