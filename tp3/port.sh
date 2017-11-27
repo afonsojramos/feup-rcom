0deba1ec505783175a727319bd3f7fc39d2732f8
msramalho <up201403027@fe.up.pt>
2017-11-27 09:08:35 +0000

    project folder organization

--- tp3/port.sh
+++
@@ -1,3 +0,0 @@
-sudo printf "Opening virtual serial port... Done.\nPress CTRL+C at any time to close it.\n";
-sudo socat PTY,link=/dev/ttyS0 PTY,link=/dev/ttyS1;
-printf "\nClosing virtual serial port... Done.\n"
